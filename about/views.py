4c080d3e8c0b3eff61c758fbcbe7374e41e9c608
Alexander Crabbe <alienzen@hotmail.com>
2024-06-21 17:44:29 +0000

    feat: add about view

--- about/views.py
+++ about/views.py
@@ -3,5 +3,5 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-def index(request):
-    return HttpResponse ("Hello, world!")
+def about_me(request):
+    return HttpResponse ("This would be the about page")
